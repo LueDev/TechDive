2216b3926ea1b0e38b9b79e7f5f9228462d7cb42
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-02-03 14:38:32 -0500

    test push to test slack integration

--- api/models/examModel.js
+++ api/models/examModel.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const examSchema = new mongoose.Schema({
   PATIENT_ID: { type: String, required: true },
-  AGE: { type: Number, required: true},
-  SEX: { type:String, required: true},
-  ZIP: { type:Number, required: true},
-  LATEST_BMI: { type:Number, required: true},
-  'LATEST WEIGHT': { type:Number, required: true},
-  png_filename: { type:String, required:true },
-  exam_id: { type:String, required: true},
-  'ICU ADMIT': { type:String}, 
-  '# ICU ADMIT': {type:Number},
-  MORTALITY: {type:String}
+  AGE: { type: Number, required: true },
+  SEX: { type: String, required: true },
+  ZIP: { type: Number, required: true },
+  LATEST_BMI: { type: Number, required: true },
+  "LATEST WEIGHT": { type: Number, required: true },
+  png_filename: { type: String, required: true },
+  exam_id: { type: String, required: true },
+  "ICU ADMIT": { type: String },
+  "# ICU ADMIT": { type: Number },
+  MORTALITY: { type: String },
 });
 
-const Exam = mongoose.model('Exam', examSchema);
+const Exam = mongoose.model("Exam", examSchema);
 
-module.exports = {Exam};
+module.exports = { Exam };
+
+//comment for testing slack github integration
