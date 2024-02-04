c4f07795f63cc1c79856880485f36d7b54bdb875
Rachelle Burgos <rachelle.burgoss@gmail.com>
2024-02-04 00:28:57 -0500

    Add basic routing setup with React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
-import { useApi } from './hooks/use-api';
+// import { useApi } from './hooks/use-api';
+
+const Home = React.lazy(() => import('./pages/Home'))
+const Exams = React.lazy(() => import('./pages/Exams'))
+const NotFound = React.lazy(() => import('./pages/NotFound'))
+// Other imported pages go here...
 
 function App() {
-  const { response } = useApi();
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {response}
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={
+            <React.Suspense fallback={ <>...</> }>
+              <Home />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="exams"
+          element={
+            <React.Suspense fallback={ <>...</> }>
+              <Exams />
+            </React.Suspense>
+          }
+        />
+        {/* Other paths go here... */}
+        <Route
+          path="*"
+          element={ <NotFound /> }
+        />
+      </Routes>
+    </ BrowserRouter>
   );
 }
 
