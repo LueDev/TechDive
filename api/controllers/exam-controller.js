e0bf51be8fe88d9e3fc35f1fc8f5acf37a917c41
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-02-10 01:52:25 -0500

    Updated exam routes in /routes/index.js to remove the authorizedToken and get all exam data without JWT while testing. Updated get exams '/' to grab 50 exams from mongoDB to test pagination. We'll add more once we space out the pagination to support all 105 exams, maybe a '...' pagination div somewhere or something? We'll figure it out

--- api/controllers/exam-controller.js
+++ api/controllers/exam-controller.js
@@ -1,65 +1,54 @@
 const jwt = require('jsonwebtoken');
 const { Exam } = require('../models/examModel');
-const { Notification } = require('../models/notificationModel')
+const { Notification } = require('../models/notificationModel');
 const amqp = require('amqplib');
 
 const getExams = async (req, res) => {
-  console.log("Exams page")
+  console.log('Exams page');
+
+  const exams = await Exam.find().limit(50);
+  // console.log(exams)
 
   return res.status(200).json({
-    success: true,
-    message: 'Exams API is working.'
-
+    exams: exams,
   });
 };
-
 
 const createExam = async (req, res) => {
-  console.log("Create Exams endpoint reached")
+  console.log('Create Exams endpoint reached');
 
-  try{
-
+  try {
     //Get the user performing this action. AuthenticateToken stored the user in req.user
-    const user = req.user
+    const user = req.user;
+  } catch {}
 
-
-
-
-
-  }catch{
-
-  }
-
-    res.status(200).json({
+  res.status(200).json({
     success: true,
-    message: 'Exams API is working.'
+    message: 'Exams API is working.',
   });
 };
 
-
 const updateExam = async (req, res) => {
-  console.log("Update Exams endpoint reached")
+  console.log('Update Exams endpoint reached');
 
   return res.status(200).json({
     success: true,
-    message: 'Exams API is working.'
+    message: 'Exams API is working.',
   });
 };
 
 const deleteExam = async (req, res) => {
-  console.log("Delete Exams endpoint reached")
+  console.log('Delete Exams endpoint reached');
 
   return res.status(200).json({
     success: true,
-    message: 'Exams API is working.'
+    message: 'Exams API is working.',
   });
 };
-
-
 
 module.exports = {
   getExams,
   createExam,
   updateExam,
-  deleteExam
+  deleteExam,
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 const ExamController = require('../controllers/exam-controller');
 
-router.get('/', authenticateToken, ExamController.getExams);
-router.get('/exam/:id', authenticateToken, ExamController.getExams); //update controller
-router.patch('/exam/:id', authenticateToken, ExamController.updateExam); //update controller
-router.delete('/exam/:id', authenticateToken, ExamController.createExam); // update controller
-router.post('/create', authenticateToken, ExamController.createExam); //update controller
+router.get('/', ExamController.getExams); //router.get('/', authenticateToken, ExamController.getExams);
+router.get('/exam/:id', ExamController.getExams); //individual exam controller with JWT: router.get('/exam/:id', authenticateToken, ExamController.getExams);
+router.patch('/exam/:id', authenticateToken, ExamController.updateExam); //update individual exam with JWT controller: router.patch('/exam/:id', authenticateToken, ExamController.updateExam);
+router.delete('/exam/:id', authenticateToken, ExamController.createExam); // delete individual exam with JWT controller: router.delete('/exam/:id', authenticateToken, ExamController.createExam);
+router.post('/create', ExamController.createExam); //create individual exam with JWT controller: router.post('/create', authenticateToken, ExamController.createExam);
 
 module.exports = router;
