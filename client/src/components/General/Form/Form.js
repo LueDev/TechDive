d39f8de9de18cc671a71d7d595fe494df237abbe
Jtothesun <jdm22@njit.edu>
2024-02-27 08:24:40 -0500

    added formik template to dynamic form component

--- client/src/components/General/Form/Form.js
+++ client/src/components/General/Form/Form.js
@@ -1,97 +1,47 @@
-/*import React from 'react';
-import './Form.css';
+import './Form.css'
 
-function Form({ formik, fields }) {
-    return (
-        <form className="form" onSubmit={formik.handleSubmit}>
+function Form({formik, fields, onSubmit}) {
+  return (
+    <>
+    <div className='form-cont'>
+        <form className="form" onSubmit={onSubmit}> 
             {fields.map(field => {
                 const fieldError = formik.touched[field.name] && formik.errors[field.name];
-                return (
-                    <div key={field.name}>
-                        {field.type === 'textarea' ? (
+                    return (
+                        <div key={field.name} className='form-fields'>
+                        {field.type === 'textarea' ? ( //if (field.type === 'textarea')
                             <textarea
                                 className="form-input"
+                                key={field.name}
                                 name={field.name}
                                 placeholder={field.placeholder}
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
-                                value={formik.values[field.name]}
-                                rows={field.rows || 3}
+                                value={formik.values[field.name]}//formData[field.name] || ''}
+                                rows={field.rows || 3} // Use the rows attribute from field definition, defaulting to 3 if not specified
                             />
-                        ) : (
+                        ): (
                             <input
                                 className="form-input"
+                                key={field.name}
                                 type={field.type || 'text'}
                                 name={field.name}
                                 placeholder={field.placeholder}
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
-                                value={formik.values[field.name]}
-                            />
+                                value={formik.values[field.name] || ''}
+                        />
                         )}
-                        {fieldError && <div className="error">{formik.errors[field.name]}</div>}
-                    </div>
-                );
-            })}
-            <button className="btn-submit" type="submit">Submit</button>
-        </form>
-    );
-};
 
-export default Form;
-*/
-
-import React, { useState} from "react";
-import './Form.css'
-
-function Form({ onSubmit, fields}) {
-    const [formData, setFormData] = useState({});
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({ ...prevState, [name]: value }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(formData);
-    };
-
-  return (
-    <>
-        <form className="form" onSubmit={handleSubmit}>
-            {fields.map(field => {
-                if (field.type === 'textarea') {
-                    return (
-                        <textarea
-                            className="form-input"
-                            key={field.name}
-                            name={field.name}
-                            placeholder={field.placeholder}
-                            onChange={handleChange}
-                            value={formData[field.name] || ''}
-                            rows={field.rows || 3} // Use the rows attribute from field definition, defaulting to 3 if not specified
-                        />
+                        {fieldError && <div className='formik-error'>{formik.errors[field.name]}</div>}
+                        </div>
                     );
-                } else {
-                    return (
-                        <input
-                            className="form-input"
-                            key={field.name}
-                            type={field.type || 'text'}
-                            name={field.name}
-                            placeholder={field.placeholder}
-                            onChange={handleChange}
-                            value={formData[field.name] || ''}
-                        />
-                    );
-                }
-            })}
-            
-    <button onSubmit={handleSubmit} className="btn-submit" type="submit">Submit</button>
+                        })}
+                <button className="btn-submit" type="submit">Submit</button>  
         </form>
-    </>
+        </div>
+        </>
     );
-};
+}
 
 export default Form;
