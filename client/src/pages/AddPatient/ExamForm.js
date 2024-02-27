c5a7e12899380cd3bc3e94ce5e552d0e59867053
Jtothesun <jdm22@njit.edu>
2024-02-27 08:16:43 -0500

    Fixed ExamForm css styles, added form validation via formik

--- client/src/pages/AddPatient/ExamForm.js
+++ client/src/pages/AddPatient/ExamForm.js
@@ -1,89 +1,10 @@
-/*import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
 import Form from '../../components/General/Form/Form.js';
-
-const ExamForm = ({ update }) => {
-    const patientFields = [
-        { name: 'patientId', placeholder: 'Patient ID'},
-        { name: 'age', placeholder: 'Age'},
-        { name: 'sex', placeholder: 'Sex'},
-        { name: 'bmi', placeholder: 'BMI'},
-        { name: 'zipCode', placeholder: 'Zip Code'},
-        { name: 'examId', placeholder: 'Exam ID'},
-        { name: 'keyFindings', placeholder: 'Key Findings', type: 'textarea'},
-        { name: 'brixiaScores', placeholder: 'Brixia Scores'},
-        { name: 'imageURL', placeholder: 'Image URL'}
-    ];
-
-    // Assigning random initial values for demonstration purposes
-    const initialValues = {
-        patientId: 'PID123', // Example random value
-        age: 30, // Example random value
-        sex: 'Male', // Example placeholder value
-        bmi: 24.5, // Example random value
-        zipCode: '12345', // Example placeholder value
-        examId: 'EXAM123', // Example random value
-        keyFindings: 'Initial key findings...', // Example placeholder text
-        brixiaScores: 'B1', // Example placeholder value
-        imageURL: 'http://example.com/image.jpg' // Example placeholder URL
-    };
-
-    // Ensure all fields are included in the validation schema and marked as required
-    const validationSchema = Yup.object({
-        patientId: Yup.string().required('Patient ID is required'),
-        age: Yup.number().positive('Must be a positive number').required('Age is required'),
-        sex: Yup.string().required('Sex is required'),
-        bmi: Yup.number().positive('Must be a positive number').required('BMI is required'),
-        zipCode: Yup.string().required('Zip Code is required'),
-        examId: Yup.string().required('Exam ID is required'),
-        keyFindings: Yup.string().required('Key Findings are required'),
-        brixiaScores: Yup.string().required('Brixia Scores are required'),
-        imageURL: Yup.string().url('Must be a valid URL').required('Image URL is required'),
-    });
-
-    const formik = useFormik({
-        initialValues,
-        validationSchema})
-        /*onSubmit: async (values, { setSubmitting, resetForm }) => {
-            try {
-                console.log('Form data:', values);
-                const response = await fetch('http://localhost:9000/exams', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(values),
-                });
-                if (!response.ok) throw new Error('Network response was not ok');
-                alert('Form submitted successfully');
-                resetForm(); // Optionally reset form fields to initial values after successful submission
-            } catch (error) {
-                console.error('Form submission error:', error);
-                alert('Form submission failed');
-            } finally {
-                setSubmitting(false);
-            }
-        },
-    });
-
-    
-    console.log('Formik values:', formik.values);
-    return (
-        <div>
-            <h2 className='patient-info-title'>Patient Info</h2>
-            <Form formik={formik} fields={patientFields} />
-        </div>
-    );
-};
-
-export default ExamForm;
-*/
-import Form from '../../components/General/Form/Form.js';
-import { data } from '../../mockdata.js';
+//import { data } from '../../mockdata.js';
 import './ExamForm.css';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 
-const ExamForm = ({update}) => {
+const ExamForm = () => {
     const patientFields = [
         { name: 'patientId', placeholder: 'Patient ID'},
         { name: 'age', placeholder: 'Age'},
@@ -98,27 +19,81 @@ const ExamForm = ({update}) => {
         { name: 'imageURL', placeholder: 'Image URL'}
     ];
 
-    const handleUserSubmit = (formData) => {
-        console.log('user Data:', formData);
+    const initialValues = patientFields.reduce((values, field) => {
+        values[field.name] = '';
+        return values;
+    }, {});
+    const formik = useFormik({
+        initialValues: initialValues,
+        validationSchema: Yup.object({
+            patientId: Yup.string().matches(
+                /^COVID-[0-9]*$/,
+                "Invalid patient ID: Must follow the COVID-<numbers> format and is case-insensitive")
+            .required('Required'),
+            age: Yup.number()
+            .typeError("Invalid input: Age must be a number")
+            .min(1, 'Age must be at least 1.')
+            .max(122, 'Age must be 122 or less.') //Based on oldest living person ever documented
+            .required('Required')
+            .positive(),
+            sex: Yup.string().matches(
+                /^(M|F|X)$/,
+                "Invalid marker: Must be M, F, or X")
+            .required('Required'),
+            bmi: Yup.number()
+            .typeError("Invalid input: BMI must be a number") //incoporate formula: bmi = weight(lb)/height^2 * 703
+            .required('Required')
+            .positive(),
+            height: Yup.number()
+            .typeError("Invalid Input: Height must be a number")
+            .min(18, 'Invalid input: Height must be at least 18 inches')
+            .max(108, 'Invalid input: Height cannot exceed 108 inches')
+            .required('Required')
+            .positive() ,
+            weight: Yup.number()
+            .min(1, "Weight must be at least 1 lb")
+            .max(1400, "Weight cannot be over 1,400 lbs")
+            .required('Required').positive(),
+            zipCode: Yup.string().matches(
+                /^[0-9]{5}$/, 
+                "Invalid zip code: Must be 5 digits long")
+            .required('Required'),
+            examId: Yup.string().matches(
+                 /^Exam-[0-9]*$/i, 
+                 "Invalid Exam ID: must follow the EXAM-<numbers> format and is case-insensitive")
+            .required('Required'),
+            keyFindings: Yup.string()
+            .required('Required'),
+            brixiaScores: Yup.string().matches(/^([0-9],[0-9])(,[0-9],[0-9]){1,2}$/, 'Brixia scores must be at least 2 numbers, at most 6, and in range 0-6 (seperated by commas)')
+            .required('Required'),
+            imageUrl: Yup.string()
+            .required('Required')
+    
+        })
+    });
+
+
+    const handleUserSubmit = (event) => {
+        event.preventDefault();
+        console.log('user Data:', formik.values);
         fetch("http://localhost:9000/create",
         {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-                body: JSON.stringify(formData),
+                body: JSON.stringify(formik.values),
         })
         .then(res => res.json())
         .then(data => console.log(data))
     };
 
 return (
-    <>
+    
     <div>
         <h2 className='patient-info-title'> Patient Info </h2>
-        <Form onSubmit={handleUserSubmit} fields={patientFields}/>
+        <Form formik={formik} fields={patientFields} onSubmit={handleUserSubmit} />
     </div>
-    </>
     );
 }
 
