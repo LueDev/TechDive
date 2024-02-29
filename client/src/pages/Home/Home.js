2ca069c8302147f19a5b957600004bd60b874825
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-02-29 17:03:37 -0500

    Added search/filter functionality

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -8,6 +8,8 @@ import { ExamContext } from '../../examcontext';
 const HomePage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const { examData, getExams } = useContext(ExamContext);
+  const [search, setSearch] = useState('');
+  const [filteredExams, setFilteredExams] = useState([]);
 
   const recordsPerPage = 15;
   const lastIndex = currentPage * recordsPerPage;
@@ -18,30 +20,63 @@ const HomePage = () => {
     getExams();
   }, []);
 
+  useEffect(() => {
+    filterExams();
+  }, [search]); // Call filterExams whenever search changes
+
   function changePage(id) {
     setCurrentPage(id);
   }
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
+  const filterExams = () => {
+    const filtered = examData.filter((value) => filterObjectBySearch(value, search));
+    setFilteredExams(filtered);
+  };
+
+  const filterObjectBySearch = (obj, search) => {
+    return Object.values(obj).some((value) => {
+      if (typeof value === 'string' && value.toLowerCase().includes(search.toLowerCase())) {
+        return true;
+      }
+      if (typeof value === 'number' && value.toString().includes(search)) {
+        return true;
+      }
+      return false;
+    });
+  };
 
   return (
     <div className="main-content">
       <h1>All Exams</h1>
       <div className="search-bar-container d-flex justify-content-end">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <InputGroup>
             <Form.Control
               placeholder="Search patient"
               className="search-input"
+              value={search}
+              onChange={handleSearchChange}
+              autoFocus
             />
           </InputGroup>
         </Form>
       </div>
 
       <div className="table-container">
-        <PatientTable records={records} />
+        <PatientTable records={search ? filteredExams : examData} />
 
         <nav>
           <PaginationComponent
-            totalRecords={examData.length}
+            totalRecords={search ? filteredExams.length : examData.length}
             recordsPerPage={recordsPerPage}
             currentPage={currentPage}
             onPageChange={changePage}
